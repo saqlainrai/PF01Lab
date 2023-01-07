dfc27adde1c0634af525a12554ced7b69ea4d874
Saqlain Mansab <saqlainmansab786@gmail.com>
2023-01-07 12:48:25 +0500

    Worked With CLI

---
+++ Installed MinGW.cpp
@@ -0,0 +1,41 @@
+
+Programming Fundamental Lab 01
+
+COMMANDS TO BE LEARNED...
+
+
+dir 		list all the directories in the current directory
+
+cd .. 	go back a directory
+
+cd / 	go to the root of the directory
+
+mkdir 	“newfile.extension” create a new directory
+
+rmdir 	“filetoberemoved.extension” remove a directory
+
+copy 	sourcefileLocation Target Copies file from source to target directory
+
+move 	sourcefileLocation Target Moves file from source to target directory
+
+move 	*.txt Target Moves all txt files from the current directory to Target directory
+
+exit 	It is used to Exit the Command Prompt Window
+
+
+
+
+move filename newfilename 	This will rename the file in the same directory
+
+del filename.fileextension 	This will delete the file from the directory
+
+type “filename.extension” 	Used to display the contents of a file
+
+
+
+LINK TO INSTALL MinGW
+
+
+https://sourceforge.net/projects/mingw/files/
+
+
